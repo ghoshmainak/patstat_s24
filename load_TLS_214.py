4a02c3f635778fd2837ea12d50ce8fe8bcec33a4
Mainak Ghosh <mainak.narendrapur@gmail.com>
2024-11-28 06:53:55 +0100

    added npl publication

---
+++ load_TLS_214.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from config import RAW_FOLDER, DATA_FOLDER
+
+
+TABLE_NAME = "tls214"
+files_list = []
+for file in RAW_FOLDER.glob(f"{TABLE_NAME}*.csv"):
+    _df = pd.read_csv(file, low_memory=False)
+    print(f"Loaded: {file}")
+    files_list.append(_df)
+df = pd.concat(files_list, ignore_index=True)
+del files_list
+print(f"shape of {TABLE_NAME}={df.shape}")
+df.to_feather(DATA_FOLDER / f"{TABLE_NAME.upper()}.feather")
